2d917896986546c9b3186fa0b4c4c97cad143336
GonzaloFdezMoreno <gonzaf05@ucm.es>
2023-03-16 11:48:14 +0100

    metidos nombres y el angulo de vista, del minotauro cometado y ajustado

--- IAV-Navegacion/Assets/Scripts/Comportamientos/Vista.cs
+++ IAV-Navegacion/Assets/Scripts/Comportamientos/Vista.cs
@@ -12,8 +12,8 @@ public class Vista : MonoBehaviour
     Transform playerTransform;
     RaycastHit sight = new RaycastHit();
 
-    
-    
+    float angvista; //para ver si te ve el minotauro
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -26,24 +26,29 @@ public class Vista : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Physics.Raycast(transform.position, playerTransform.position - transform.position,out sight))
+        if(Physics.Raycast(transform.position, playerTransform.position - transform.position,out sight)) //creamos una linea entre el jugador y el minotauro
         {
 
-            Debug.Log("Ray hit: " + sight.collider.gameObject.tag);
-            if (sight.collider.gameObject.tag == "Player") //hay que comprobar tambien el angulo de vista
-            {
-                //asignar al jugador 
-                mero.enabled = false;
-                lleg.enabled = true;
-                lleg.objetivo = sight.collider.gameObject;
-                
+            angvista = Vector3.Angle(transform.forward, playerTransform.position - transform.position); //calculamos el angulo entre la direccion que lleva el minotauro y el raycast creado
 
+            Debug.Log("Ray hit: " + sight.collider.gameObject.tag);
+            if (sight.collider.gameObject.tag == "Player"&&angvista>-30&&angvista<30) //comprobamos que no haya nada entre player y el minotauro y ademas que esté en un angulo bajo de forma que pueda ver al jugador
+            {
+                if (!lleg.enabled) { 
+                    //si lo ve que lo persiga
+                    mero.enabled = false;
+                    lleg.enabled = true;
+                    lleg.objetivo = sight.collider.gameObject;
+
+                }
             }
             else
             {
-                //cambiar pesos para que merodee
-                mero.enabled = true;
-                lleg.enabled = false;
+                if (lleg.enabled) { //para que solo lo haga 1 vez
+                    //si no lo ve que siga merodeando
+                    mero.enabled = true;
+                    lleg.enabled = false;
+                }
             }
         }
 
